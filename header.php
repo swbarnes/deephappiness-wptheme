f8af53afaaa73ba285924bfb5529aa18350956e9
Shaun Barnes <shaun.w.barnes@gmail.com>
2016-06-16 15:08:04 -0400

    Edited header.php to include custom Lotus header and dynamic menu

--- header.php
+++ header.php
@@ -24,27 +24,42 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'deephappiness' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+	<!-- Header -->
+	<header class="site-header" role="banner">
+		<div id="top-banner">
+			<div class="container">
+				<a href="/"><img src="img/top-logo.png" alt="Deep Happiness; An inside-out path toward living fully." class="top-logo"></a>
+			</div><!-- .container -->
+		</div><!-- #top-banner -->
+		<nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+			<div class="container">
+				<!-- Brand and toggle mobile button -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#dh-menu" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div><!-- .navbar-header -->
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
+				<!-- Nav links collection that will be collapsed -->
+				<?php
+					// If the menu (WP admin area) is not set, then the "menu_class" is applied to "container". In other words, it overwrites the "container_class". Ref: http://wordpress.org/support/topic/wp_nav_menu-menu_class-usage-bug?replies=4
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'deephappiness' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+					wp_nav_menu(
+						array(
 
-	<div id="content" class="site-content">
+							'theme_location'	=> 'primary',
+							'container'			=> 'div',
+							'container_class'	=> 'collapse navbar-collapse',
+							'container_id'		=> 'dh-menu',
+							'menu_class'		=> 'nav navbar-nav navbar-left',
+
+						)
+					);
+				?>
+
+			</div><!-- .container -->
+		</nav><!-- .navbar -->
+	</header><!-- header -->
