7badb907097c4620d438c548665d36d99783ff49
Shaun Barnes <shaun.w.barnes@gmail.com>
2016-06-20 13:59:25 -0400

    Added div.comment-wrap and changed h2's to h3 in comments.php

--- comments.php
+++ comments.php
@@ -21,23 +21,23 @@ if ( post_password_required() ) {
 ?>
 
 <div id="comments" class="comments-area">
+	<div class="comments-wrap">
 
 	<?php
-	// You can start editing here -- including this comment!
 	if ( have_comments() ) : ?>
-		<h2 class="comments-title">
+		<h3 class="comments-title">
 			<?php
 				printf( // WPCS: XSS OK.
-					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'deephappiness' ) ),
+					esc_html( _nx( 'One comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'deephappiness' ) ),
 					number_format_i18n( get_comments_number() ),
 					'<span>' . get_the_title() . '</span>'
 				);
 			?>
-		</h2>
+		</h3>
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'deephappiness' ); ?></h2>
+			<h3 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'deephappiness' ); ?></h3>
 			<div class="nav-links">
 
 				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'deephappiness' ) ); ?></div>
@@ -58,7 +58,7 @@ if ( post_password_required() ) {
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'deephappiness' ); ?></h2>
+			<h3 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'deephappiness' ); ?></h3>
 			<div class="nav-links">
 
 				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'deephappiness' ) ); ?></div>
@@ -82,4 +82,5 @@ if ( post_password_required() ) {
 	comment_form();
 	?>
 
+	</div><!-- comments-wrap -->
 </div><!-- #comments -->
