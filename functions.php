49e241bdbc1255e3c2ce7e2462486cd2c5112edc
Shaun Barnes <shaun.w.barnes@gmail.com>
2016-06-20 13:19:39 -0400

    Fixed headings for widgets sidebar in functions.php

--- functions.php
+++ functions.php
@@ -92,8 +92,8 @@ function deephappiness_widgets_init() {
 		'description'   => esc_html__( 'Add widgets here.', 'deephappiness' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+		'before_title'  => '<h4 class="widget-title">',
+		'after_title'   => '</h4>',
 	) );
 }
 add_action( 'widgets_init', 'deephappiness_widgets_init' );
