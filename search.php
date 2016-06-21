6a339631533ab6e97fcda0d0d66caeec5262cb6e
Shaun Barnes <shaun.w.barnes@gmail.com>
2016-06-21 13:58:27 -0400

    Edited the search results page to format results

--- search.php
+++ search.php
@@ -9,40 +9,46 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php
-		if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'deephappiness' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
+	<div class="container">
+		<div class="row" id="primary">
+				
+			<main id="content" class="col-sm-8" role="main">
 
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+			if ( have_posts() ) : ?>
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+				<div class="page-header">
+					<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'deephappiness' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+				</div><!-- .page-header -->
 
-			endwhile;
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) : the_post();
 
-			the_posts_navigation();
+					/**
+					 * Run the loop for the search to output the results.
+					 * If you want to overload this in a child theme then include a file
+					 * called content-search.php and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', 'search' );
 
-		else :
+				endwhile;
 
-			get_template_part( 'template-parts/content', 'none' );
+				the_posts_navigation();
 
-		endif; ?>
+			else :
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+				get_template_part( 'template-parts/content', 'none' );
+
+			endif; ?>
+
+			</main><!-- #main -->
+
+			<aside class="col-sm-4">
+				<?php get_sidebar(); ?>
+			</aside>
+		</div><!-- row#primary -->
+	</div><!-- container -->
 
 <?php
-get_sidebar();
 get_footer();
--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -11,20 +11,28 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php deephappiness_posted_on(); ?>
-		</div><!-- .entry-meta -->
+		<div class="post-details">
+			<i class="fa fa-user"></i> <?php the_author(); ?>
+			<i class="fa fa-clock-o"></i> <time><?php the_date(); ?></time>
+			<i class="fa fa-folder-open"></i> <?php the_category( ', ' ); ?>
+			<?php the_tags( '<i class="fa fa-tags"></i> ', ', ', '' ); ?>
+			<i class="fa fa-comments"></i> <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a>
+
+			<?php edit_post_link( 'Edit', '<i class="fa fa-pencil"></i> ', '' ); ?>
+		</div><!-- post-details -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-summary">
+	<?php if ( has_post_thumbnail() ) : ?>
+	<div class="post-image">
+		<?php the_post_thumbnail(); ?>
+	</div><!-- post-image -->
+	<?php endif; ?>
+
+	<div class="post-excerpt">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
-
-	<footer class="entry-footer">
-		<?php deephappiness_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
